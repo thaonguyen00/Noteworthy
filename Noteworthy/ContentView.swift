d105215ecd2b3e0286a71f0317cbc60e9131a689
Thao Nguyen <thao.nguyen@zaplabs.com>
2019-11-02 19:21:33 -0700

    Initial Commit

---
+++ Noteworthy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Noteworthy
+//
+//  Created by Thao Nguyen on 11/2/19.
+//  Copyright © 2019 Thao Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
